ee3ac715690ad9b1152c4c5c93717e52e5743f3e
Jonas Stiefer <neo.stief.jon@gmail.com>
2026-03-26 16:58:56 +0100

    layout optimazition

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,15 @@
 function Footer() {
   return (
-    <footer className="flex justify-around items-end">
-      <nav aria-label="Footer navigation" className="flex flex-col">
-        <h2>Contact me</h2>
-        <a href="mailto:#">Email</a>
-        <a href="#">Linkedin</a>
-        <a href="#">GitHub</a>
-      </nav>
-      <p>© 2025 Jonas Stiefer. All rights reserved.</p>
+    <footer className="w-full">
+      <div className="max-w-7xl w-full mx-auto flex justify-around items-end">
+        <nav aria-label="Footer navigation" className="flex flex-col">
+          <h2>Contact me</h2>
+          <a href="mailto:#">Email</a>
+          <a href="#">Linkedin</a>
+          <a href="#">GitHub</a>
+        </nav>
+        <p>© 2025 Jonas Stiefer. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,25 @@ import DarkModeSwitch from "./DarkModeSwitch";
 
 function Header() {
   return (
-    <header className="border-b-1 border-(--color-text-rgba) fixed w-full flex justify-around items-center px-10 py-6 z-10 backdrop-blur-md">
-      <Logo className="h-15" role="img" aria-label="Portfolio logo" />
-      <nav aria-label="Main navigation" className="flex gap-6">
-        <a className="hover:border-b-1 transition-all" href="#about">
-          About
-        </a>
-        <a className="hover:border-b-1 transition-all" href="#skills">
-          Skills
-        </a>
-        <a className="hover:border-b-1 transition-all" href="#projects">
-          Projects
-        </a>
-        <a className="hover:border-b-1 transition-all" href="#contact">
-          Contact
-        </a>
-      </nav>
-      <DarkModeSwitch />
+    <header className="border-b-1 border-(--color-text-rgba) sticky top-0 w-full z-10 backdrop-blur-md">
+      <div className="max-w-7xl w-full mx-auto flex justify-around items-center px-10 py-6">
+        <Logo className="h-15" role="img" aria-label="Portfolio logo" />
+        <nav aria-label="Main navigation" className="flex gap-6">
+          <a className="hover:border-b-1 transition-all" href="#about">
+            About
+          </a>
+          <a className="hover:border-b-1 transition-all" href="#skills">
+            Skills
+          </a>
+          <a className="hover:border-b-1 transition-all" href="#projects">
+            Projects
+          </a>
+          <a className="hover:border-b-1 transition-all" href="#contact">
+            Contact
+          </a>
+        </nav>
+        <DarkModeSwitch />
+      </div>
     </header>
   );
 }
