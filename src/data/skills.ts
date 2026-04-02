7dc3c2b4a5529556fa6a1afd8c14f7e4607384f6
Jonas Stiefer <neo.stief.jon@gmail.com>
2026-04-02 12:44:58 +0200

    fix icons ftp server problem

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,70 +1,88 @@
+import javascriptIcon from "../assets/icons/javascript.svg";
+import typescriptIcon from "../assets/icons/typescript.svg";
+import pythonIcon from "../assets/icons/python.svg";
+import css3Icon from "../assets/icons/css3.svg";
+import sassIcon from "../assets/icons/sass.svg";
+import html5Icon from "../assets/icons/html5.svg";
+import angularjsIcon from "../assets/icons/angularjs.svg";
+import reactIcon from "../assets/icons/react.svg";
+import djangoIcon from "../assets/icons/django.svg";
+import tailwindIcon from "../assets/icons/tailwind.svg";
+import gitIcon from "../assets/icons/git.svg";
+import viteIcon from "../assets/icons/vite.svg";
+import restApiIcon from "../assets/icons/rest-api.svg";
+import sqliteIcon from "../assets/icons/sqlite.svg";
+import postgresqlIcon from "../assets/icons/postgresql.svg";
+import redisIcon from "../assets/icons/redis.svg";
+import linuxIcon from "../assets/icons/linux.svg";
+
 export const skills = [
   {
     name: "Javascript",
-    icon: "/icons/javascript.svg",
+    icon: javascriptIcon,
   },
   {
     name: "Typescript",
-    icon: "/icons/typescript.svg",
+    icon: typescriptIcon,
   },
   {
     name: "Python",
-    icon: "/icons/python.svg",
+    icon: pythonIcon,
   },
   {
     name: "CSS",
-    icon: "/icons/css3.svg",
+    icon: css3Icon,
   },
   {
     name: "SASS",
-    icon: "/icons/sass.svg",
+    icon: sassIcon,
   },
   {
     name: "HTML",
-    icon: "/icons/html5.svg",
+    icon: html5Icon,
   },
   {
     name: "Angular",
-    icon: "/icons/angularjs.svg",
+    icon: angularjsIcon,
   },
   {
     name: "React",
-    icon: "/icons/react.svg",
+    icon: reactIcon,
   },
   {
     name: "Django",
-    icon: "/icons/django.svg",
+    icon: djangoIcon,
   },
   {
     name: "Tailwind",
-    icon: "/icons/tailwind.svg",
+    icon: tailwindIcon,
   },
   {
     name: "Git",
-    icon: "/icons/git.svg",
+    icon: gitIcon,
   },
   {
     name: "Vite",
-    icon: "/icons/vite.svg",
+    icon: viteIcon,
   },
   {
     name: "REST-Api",
-    icon: "/icons/rest-api.svg",
+    icon: restApiIcon,
   },
   {
     name: "SQLite",
-    icon: "/icons/sqlite.svg",
+    icon: sqliteIcon,
   },
   {
     name: "PostgreSQL",
-    icon: "/icons/postgresql.svg",
+    icon: postgresqlIcon,
   },
   {
     name: "Redis",
-    icon: "/icons/redis.svg",
+    icon: redisIcon,
   },
   {
     name: "Linux",
-    icon: "/icons/linux.svg",
+    icon: linuxIcon,
   },
 ];
